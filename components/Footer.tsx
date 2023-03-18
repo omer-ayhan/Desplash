77b253c473faeac985dc0799afa706f08eb15fc8
omer-ayhan <om.ayhan247@gmail.com>
2023-03-18 10:47:29 +0300

    feat: designed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
 import React from "react";
+import { Divider } from "./Divider";
 
 export function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className="w-screen p-5 px-20">
+			<Divider className="my-3" />
+			<div className="flex justify-between">
+				<Link href="/" className="text-xl font-bold">
+					Desplash
+				</Link>
+
+				<p className="text-sm text-gray-500">
+					© 2023 Desplash. All rights reserved. Made with{" "}
+					<span className="text-red-500">❤</span>
+				</p>
+			</div>
+		</div>
+	);
 }
